6d0c1ecb8592e669f37940d6f86ba66835b227de
Albert <vetusbs@gmail.com>
2015-07-12 23:56:53 +0200

    second fail test

--- test/RotateArrayTest.java
+++ test/RotateArrayTest.java
@@ -19,8 +19,17 @@ public class RotateArrayTest {
         RotateArray rotateArray = new RotateArray(1,5);
 
         int[] rotatedArray = rotateArray.rotate();
+        
+        assertEquals(0, rotatedArray[0]);
+    }
+    
+    @Test public void shouldReturnReverseArrayWhenNIsTwoAndKIsOne() {
+        RotateArray rotateArray = new RotateArray(2,1);
 
-        assertEquals(rotatedArray[0], 1);
+        int[] rotatedArray = rotateArray.rotate();
+        
+        assertEquals(1, rotatedArray[0]);
+        assertEquals(0, rotatedArray[1]);
     }
 
 }
